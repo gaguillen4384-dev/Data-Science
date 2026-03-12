88bdd24a9ee35bdd2aa00766f5bd315cce3f8a8c
gaguillen4384-dev <gaguillen4384.dev@gmail.com>
2026-03-12 16:23:25 -0400

    Starting Treebased assignment

---
+++ DataMining_2/Tree_Methods_Analysis_Bank_Marketing/Code/DataPreparation.R
@@ -0,0 +1,27 @@
+# Load necessary library
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(caret)
+
+
+data_full <- read.csv("./Dataset/bank/bank-full.csv", stringsAsFactors = TRUE)
+
+
+target_col <- "target" 
+
+
+set.seed(42) # Sets a seed so your results are reproducible
+train_index <- createDataPartition(data_full[[target_col]], p = 0.9, list = FALSE)
+
+
+train_data <- data_full[train_index, ]
+test_data  <- data_full[-train_index, ]
+
+
+write.csv(train_data, "./Dataset/train_split_90.csv", row.names = FALSE)
+write.csv(test_data, "./Dataset/test_split_10.csv", row.names = FALSE)
+
+
+cat("Split Complete!\n")
+cat("Training rows:", nrow(train_data), "\n")
+cat("Testing rows:", nrow(test_data), "\n")
+
---
+++ DataMining_2/Tree_Methods_Analysis_Bank_Marketing/README.md
@@ -0,0 +1,11 @@
+Performed: 
+    Spring 2026
+
+Project: 
+    TBD
+
+Data: 
+    Included in repo (source: https://archive-beta.ics.uci.edu/dataset/222/bank+marketing)
+
+Synopsis:
+    TBD
